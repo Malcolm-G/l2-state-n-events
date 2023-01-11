79946f999e26fe4cbc855f2c6a3d1e5e9a7955d8
Malcolm-G <malcolm.githuka@gmail.com>
2023-01-11 12:17:42 +0300

    added one bad state and one appropriate state

--- src/components/CountMiddle.js
+++ src/components/CountMiddle.js
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 
 function CountMiddle(){
 
+    const [count,setCount] = useState(0);
+
+    function clicked(){
+        setCount(count+1);
+    }
+
     return(
             <div>
                 <center>
-                    <h4 className="btn-text">0</h4>
-                    <button className="timeButton">CLICK ME</button>
+                    <h4 className="btn-text">{count}</h4>
+                    <button
+                    className="timeButton"
+                    onClick={()=>setCount(count+1) /*or you could just call clicked*/}>CLICK ME</button>
                 </center>
             </div>
     )
